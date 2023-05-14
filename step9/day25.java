95e8b3ede215e25f7a3b89a96b73958057a1c5bb
pjs <vkrwltn12@gmail.com>
2023-05-14 23:45:48 +0900

    25day

---
+++ step9/day25.java
@@ -0,0 +1,14 @@
+package step4;
+/*
+ *   멀티 스레드 : 메인 스레드 + 영상 스레드 + 음향 스레드 => 동영상 프로세스
+ */
+public class TestThread4 {
+    public static void main(String[] args) {
+        VideoWorker videoWorker=new VideoWorker();
+        Thread videoThread=new Thread(videoWorker,"봉준호");
+        videoThread.start();
+        Thread audioThread=new Thread(new AudioWorker(),"아이유");
+        audioThread.start();
+        System.out.println("main thread 종료");
+    }
+}
